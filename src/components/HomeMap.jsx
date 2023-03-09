2d754763bbfc376f65cb4935937721fb8d81d51f
Arik Alexandrov <arikxl@gmail.com>
2023-03-10 01:12:22 +0200

    the new thing

--- src/components/HomeMap.jsx
+++ src/components/HomeMap.jsx
@@ -46,8 +46,10 @@ export default function HomeMap() {
                 defaultZoom={defaultProps.zoom}
             >
                 <AnyReactComponent
-                    lat={59.955413}
-                    lng={30.337844}
+                    lat={31.256791}
+                    lng={34.769960}
+                    // lat={59.955413}
+                    // lng={30.337844}
                     text="My Marker"
                 />
             </GoogleMapReact>
